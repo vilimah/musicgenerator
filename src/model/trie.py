c37823db4b1b7859ffdea4969df9cee3d89f6e0e
vilimah <vilimahonen@icloud.com>
2025-07-19 19:23:01 +0300

    Testien lisäys trie tietorakenteelle

--- src/model/trie.py
+++ src/model/trie.py
@@ -1,4 +1,4 @@
-class TrieSolmu:
+class Solmu:
     """ Trie tietorakenne
     """
     def __init__(self):
@@ -6,15 +6,15 @@ class TrieSolmu:
         self.paate = False # onko päätepiste vai ei
         self.arvo = None # lisäarvo
 
-class MelodiaSolmu:
+class MelodiaTrie:
     def __init__(self):
-        self.juuri = TrieSolmu() # juurisolmu
+        self.juuri = Solmu() # juurisolmu
 
     def insertti(self, avain, arvo=None):
         solmu = self.juuri
         for symboli in avain: # käy läpi syötteen
             if symboli not in solmu.lapset: # tässä luodaan uusi solmu jos symbolia ei ole
-                solmu.lapset[symboli] = TrieSolmu()
+                solmu.lapset[symboli] = Solmu()
             solmu = solmu.lapset[symboli]
         solmu.arvo = arvo
         solmu.paate = True
